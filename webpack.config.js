195e966c101633f9ebd4ec2bab28c60e096dc947
pablo Giani <pgiani@gmail.com>
2020-01-26 11:35:01 -0700

    favicon loads

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: './src/index.html',
   filename: './index.html',
+});
+const Favicon = new FaviconsWebpackPlugin({
+  logo: './src/img/logo.png', // svg works too!
+  mode: 'webapp', // optional can be 'webapp' or 'light' - 'webapp' by default
+  devMode: 'webapp', // optional can be 'webapp' or 'light' - 'light' by default
+  favicons: {
+    appName: 'Sprinler',
+    appDescription: 'Sprinler Control',
+    developerName: 'pgiani',
+    developerURL: null, // prevent retrieving from the nearest package.json
+    background: '#2196f3',
+    theme_color: '#ffffff',
+    icons: {
+      coast: false,
+      yandex: false,
+    },
+  },
 });
 
 module.exports = {
@@ -39,7 +57,11 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
     ],
   },
-  plugins: [htmlPlugin],
+  plugins: [htmlPlugin, Favicon],
 };
